1283a7c7424e28e6d3176a5c32056191b7f950d7
LongoriaYou <LongoriaYou@tutorabc.com>
2016-10-04 17:43:13 +0800

    include SmartAdmin layout and use it

--- NewUltimusWeb/Global.asax.cs
+++ NewUltimusWeb/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
 
 namespace NewUltimusWeb
 {
@@ -12,7 +13,9 @@ namespace NewUltimusWeb
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
